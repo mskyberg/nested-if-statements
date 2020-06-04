6dc0e89622d59b469157fd4901daf3c4da761380
mskyberg <mskyberg@gmail.com>
2020-06-04 17:23:34 -0500

    added some logic to make less than 10 unit test pass

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -2,12 +2,29 @@
 Program: coupon_calculations.py
 Author: Michael Skyberg, mskyberg@dmacc.edu
 Last date modified: June 2020
-Purpose:
+Purpose: demonstrate the use of a nested if statement
 """
 
+SALES_TAX = 0.06
 
+
+# calculate the order total
 def calculate_price(price, cash_coupon, percent_coupon):
-    pass
+    # calculate total with cash_coupon
+    item_total = price - cash_coupon
+    # calculate total with percent_coupon
+    item_total = (1 - (percent_coupon/100)) * item_total
+    # calculate total with shipping cost
+    item_total += get_shipping_cost(item_total)
+    # calculate total with tax
+    item_total = (1 + SALES_TAX) * item_total
+    return item_total
+
+
+# get the shipping cost based on total price
+def get_shipping_cost(price):
+    if price < 10:
+        return 5.95
 
 
 if __name__ == '__main__':
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -2,7 +2,7 @@
 Program: test_coupon_calculations.py
 Author: Michael Skyberg, mskyberg@dmacc.edu
 Last date modified: June 2020
-Purpose:
+Purpose: test the nested if statement as well as assertAlmostEqual
 """
 
 import unittest
@@ -11,12 +11,11 @@ from store import coupon_calculations as cc
 
 
 class MyTestCase(unittest.TestCase):
-    # def calculate_price(price, cash_coupon, percent_coupon):
     def test_price_under_10(self):
-        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 10), 9.16, places=4)
-        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 15), 9.00, places=4)
-        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 20), 8.84, places=4)
-        self.assertAlmostEqual(cc.calculate_price(7.99, 10, 10), 4.39, places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 10), 9.16, places=2)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 15), 9.00, places=2)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 20), 8.84, places=2)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 10, 10), 4.39, places=2)
 
 
 if __name__ == '__main__':
