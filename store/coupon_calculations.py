f90c16b54c197fc56aec4341c07bb258172a7596
mskyberg <mskyberg@gmail.com>
2020-06-04 16:32:29 -0500

    added first test case

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -6,5 +6,9 @@ Purpose:
 """
 
 
+def calculate_price(price, cash_coupon, percent_coupon):
+    pass
+
+
 if __name__ == '__main__':
     pass
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -5,8 +5,19 @@ Last date modified: June 2020
 Purpose:
 """
 
+import unittest
+import unittest.mock as mock
 from store import coupon_calculations as cc
 
 
+class MyTestCase(unittest.TestCase):
+    # def calculate_price(price, cash_coupon, percent_coupon):
+    def test_price_under_10(self):
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 10), 9.16, places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 15), 9.00, places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, 20), 8.84, places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 10, 10), 4.39, places=4)
+
+
 if __name__ == '__main__':
-    pass
+    unittest.main()
