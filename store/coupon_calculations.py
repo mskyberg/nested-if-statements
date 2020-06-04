8f0fe83cfdbde2c50256f4624f69809cbfb839d5
mskyberg <mskyberg@gmail.com>
2020-06-04 14:21:05 -0500

    cleaned up dirs

---
+++ store/coupon_calculations.py
@@ -0,0 +1,10 @@
+"""
+Program: coupon_calculations.py
+Author: Michael Skyberg, mskyberg@dmacc.edu
+Last date modified: June 2020
+Purpose:
+"""
+
+
+if __name__ == '__main__':
+    pass
---
+++ test/test_coupon_calculations.py
@@ -0,0 +1,12 @@
+"""
+Program: test_coupon_calculations.py
+Author: Michael Skyberg, mskyberg@dmacc.edu
+Last date modified: June 2020
+Purpose:
+"""
+
+from store import coupon_calculations as cc
+
+
+if __name__ == '__main__':
+    pass
